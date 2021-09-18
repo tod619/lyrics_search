04cf350169c38bad6be833d4648a16dcecd3d231
tod619 <trevorodwyer619@hotmail.com>
2021-09-18 15:18:24 +0100

    add variables in script.js file

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const result = document.getElementById('result')
+const more = document.getElementById('more')
+
+const apiURL = 'https://api.lyrics.ovh'
