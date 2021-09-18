620d6fb09dfc565edce4ee2956fa84a98a57c8d6
tod619 <trevorodwyer619@hotmail.com>
2021-09-18 15:45:19 +0100

    create async func searchSongs in script.js file

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ const result = document.getElementById('result')
 const more = document.getElementById('more')
 
 const apiURL = 'https://api.lyrics.ovh'
+
+// Search songs
+async function searchSongs(term) {
+    const res = await fetch(`${apiURL}/suggest/${term}`)
+    const data = await res.json()
+
+    showData(data)
+}
+
+function showData(data) {}
+
+// EventListners
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value.trim()
+
+
+    if(!searchTerm) {
+        alert('Please type in a search term!')
+    } else {
+        searchSongs(searchTerm)
+    }
+    
+})
+
